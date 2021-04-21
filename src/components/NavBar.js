c09d9793ab3eb127385c6008a36f08aa581e17ec
Brad Simpson <bradsimpson@icloud.com>
2021-04-21 18:19:04 -0400

    initial commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./NavBar.css";
+
+const NavBar = () => {
+
+    return(
+        <div className="navbar">
+            <h1 className='navbar-title'>React To Do App</h1>
+        </div>
+    )
+}
+
+export default NavBar;
